d9a7d6ee97e93916a996fc27282caff666e14eaa
Ericdid <ericdidom@gmail.com>
2023-10-03 00:33:34 +0200

    add.New_folder(Movie_db)

---
+++ Movie_data/Movie_db.php
@@ -0,0 +1,19 @@
+<?php
+
+  $movies = [
+    [
+    "title" => "Trappola Di Cristallo - Die Hard",
+    "genre" => ["Natalizio,Action,Triller"],
+    "length" => 132
+    ],
+    [
+    "title" => "Il Signore Degli Anelli",
+    "genre" => ["Fantasy,Avventura"],
+    "length" => 132
+    ],
+    [
+    "title" => "Star Wars",
+    "genre" => ["Fantasy,Action,Avventura"],
+    "length" => 132
+    ],
+  ];
--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
 
 require_once __DIR__ . "/Models/movie.php";
+require __DIR__ . "/Movie_data/Movie_db.php";
 
-$new_movie = new movie ("Trappola Di Cristallo - Die Hard",["Natalizio,Action,Triller"],132);
 
-var_dump($new_movie);
+var_dump($movies);
+
+// $new_movie = new movie ();
